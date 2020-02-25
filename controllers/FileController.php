c4f588965125a3ccc003330ed8a6c14685cc081d
LLITUPLEC <46906379+LLITUPLEC@users.noreply.github.com>
2020-02-26 00:42:11 +0300

    [Задача #6]: Блок документации - добавлен датаПровайдер у контроллреа FileController.php

--- controllers/FileController.php
+++ controllers/FileController.php
@@ -32,16 +32,33 @@ class FileController extends Controller
 
     public function actionIndex()
     {
+        $query = File::find();
+
+        $provider = new ActiveDataProvider([
+            'query' => $query,
+            'pagination' => [
+                'validatePage' => false,
+            ],
+        ]);
+
+        return $this->render('index', [
+            'provider' => $provider,
+        ]);
+    }
+
+    public function actionUpload()
+    {
+//        var_dump();
         $model = new File();
 
         if (Yii::$app->request->isPost) {
             $model->path = UploadedFile::getInstance($model, 'path');
-            if ($model->load(Yii::$app->request->post())) {
+            if ($model->load(Yii::$app->request->post()) && $model->validate()) {
                 Yii::$app->session->setFlash('success', 'Изображение загружено');
                 $model->save();
-                return $this->render('index', ['model' => $model]);
+                return $this->render('upload', ['model' => $model]);
             }
         }
-        return $this->render('index', ['model' => $model]);
+        return $this->render('upload', ['model' => $model]);
     }
 }
