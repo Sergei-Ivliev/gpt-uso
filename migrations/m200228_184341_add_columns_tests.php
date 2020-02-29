b20e3865efc0fd5f68b3e7d2722059d5137b1e09
Littlebear <sergei.v.ivliev@gmail.com>
2020-02-29 10:27:31 +0300

    [Задача #17: Модификация связи теста и результатов] Миграция на таблицу tests

---
+++ migrations/m200228_184341_add_columns_tests.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200228_184341_add_columns_tests
+ */
+class m200228_184341_add_columns_tests extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m200228_184341_add_columns_tests cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m200228_184341_add_columns_tests cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
