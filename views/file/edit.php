0ca2ec1605890afa9a85eef6292b057313d2c0d6
LLITUPLEC <46906379+LLITUPLEC@users.noreply.github.com>
2020-02-29 15:38:17 +0300

    [Задача #6]: Блок документации - add view edit

---
+++ views/file/edit.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @var $this yii\web\View
+ * @var $model File
+ */
+
+use app\models\File;
+use yii\helpers\Html;
+
+?>
+<div class="row">
+    <h1><?= Html::encode($model->id ? $model->title : 'Новый документ') ?></h1>
+
+    <div class="form-group pull-right">
+        <?= Html::a('Отмена', ['file/index'], ['class' => 'btn btn-info']) ?>
+    </div>
+</div>
+
+<?= $this->render('upload', [
+    'model' => $model,
+]) ?>
